d9876900ee158425a2631fc0a849fb74bd76a8d5
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2013-01-15 14:24:54 +0100

    Change text colors.

--- Classes/LinphoneUI/UILinphone.h
+++ Classes/LinphoneUI/UILinphone.h
@@ -19,8 +19,8 @@
 
 #import <UIKit/UIColor.h>
 
-#define LINPHONE_MAIN_COLOR [UIColor colorWithRed:207.0f/255.0f green:76.0f/255.0f blue:41.0f/255.0f alpha:1.0f]
-#define LINPHONE_TABLE_CELL_BACKGROUND_COLOR [UIColor colorWithRed:207.0f/255.0f green:76.0f/255.0f blue:41.0f/255.0f alpha:1.0f]
+#define LINPHONE_MAIN_COLOR [UIColor colorWithRed:62.0f/255.0f green:117.0f/255.0f blue:132.0f/255.0f alpha:1.0f]
+#define LINPHONE_TABLE_CELL_BACKGROUND_COLOR [UIColor colorWithRed:62.0f/255.0f green:117.0f/255.0f blue:132.0f/255.0f alpha:1.0f]
 
 @interface UIColor (LightAndDark)
 
